737fbdafdc58108eeb532fbb37906e74f6fd7604
Bowie375 <2200013174@stu.pku.edu.cn>
2025-09-06 11:20:50 +0800

    implement click link feature in viz.py

--- src/myurdfpy/viz.py
+++ src/myurdfpy/viz.py
@@ -1,92 +1,12 @@
-import sys
-import time
 import logging
+from functools import partial
 
 import viser
-import numpy as np
 import trimesh.transformations as tra
 
 from myurdfpy import URDF
 
 _logger = logging.getLogger(__name__)
-
-
-def setup_logging(loglevel):
-    """Setup basic logging.
-
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
-    )
-
-
-def generate_joint_limit_trajectory(urdf_model: URDF, loop_time: float):
-    """Generate a trajectory for all actuated joints that interpolates between joint limits.
-    For continuous joint interpolate between [0, 2 * pi].
-
-    Args:
-        urdf_model (yourdfpy.URDF): _description_
-        loop_time (float): Time in seconds to loop through the trajectory.
-
-    Returns:
-        dict: A dictionary over all actuated joints with list of configuration values.
-    """
-    trajectory_via_points = {}
-    for joint_name in urdf_model.actuated_joint_names:
-        if urdf_model.joint_map[joint_name].type.lower() == "continuous":
-            via_point_0 = 0.0
-            via_point_2 = 2.0 * np.pi
-            via_point_1 = (via_point_2 - via_point_0) / 2.0
-        else:
-            limit_lower = (
-                urdf_model.joint_map[joint_name].limit.lower
-                if urdf_model.joint_map[joint_name].limit.lower is not None
-                else -np.pi
-            )
-            limit_upper = (
-                urdf_model.joint_map[joint_name].limit.upper
-                if urdf_model.joint_map[joint_name].limit.upper is not None
-                else +np.pi
-            )
-            via_point_0 = limit_lower
-            via_point_1 = limit_upper
-            via_point_2 = limit_lower
-
-        trajectory_via_points[joint_name] = np.array(
-            [
-                via_point_0,
-                via_point_1,
-                via_point_2,
-            ]
-        )
-    times = np.linspace(0.0, 1.0, int(loop_time * 100.0))
-    bins = np.arange(3) / 2.0
-
-    # Compute alphas for each time
-    inds = np.digitize(times, bins, right=True)
-    inds[inds == 0] = 1
-    alphas = (bins[inds] - times) / (bins[inds] - bins[inds - 1])
-
-    # Create the new interpolated trajectory
-    trajectory = {}
-    for k in trajectory_via_points:
-        trajectory[k] = (
-            alphas * trajectory_via_points[k][inds - 1]
-            + (1.0 - alphas) * trajectory_via_points[k][inds]
-        )
-
-    return trajectory
-
-
-def viewer_callback(scene, urdf_model, trajectory, loop_time):
-    frame = int(100.0 * (time.time() % loop_time))
-    cfg = {k: trajectory[k][frame] for k in trajectory}
-
-    urdf_model.update_cfg(configuration=cfg)
-
          
 class Visualizer:
     def __init__(
@@ -99,41 +19,67 @@ class Visualizer:
         self._server = viser.ViserServer()
         self._use_collision_mesh = use_collision_mesh
 
-        self._mesh_handles = {}
-        self._frame_handles = {}
-
-        self._mesh_colors = {}
-        self._highlighted_handle = None
+        self._mesh_handles: dict[str, viser.MeshHandle] = {}
+        self._frame_handles: dict[str, viser.FrameHandle] = {}
+        self._slider_handles: dict[str, viser.SliderHandle] = {}
 
     def _add_mesh(self):
         scene = self._urdf_model.collision_scene if self._use_collision_mesh else self._urdf_model.scene
 
-        with self._server.gui.add_folder("Meshs"):
-            # Add all meshes from the scene
-            for i, geometry_name in enumerate(scene.geometry):
-                with self._server.gui.add_folder(f"{i}"):
-                    mesh = scene.geometry[geometry_name]
-                    transform = scene.graph.get(geometry_name)[0]
+        for i, geometry_name in enumerate(scene.geometry):
+            mesh = scene.geometry[geometry_name]
+            transform = scene.graph.get(geometry_name)[0]
 
-                    handle = self._server.scene.add_mesh_trimesh(
-                        name=f"mesh_{i}",
-                        mesh=mesh,
-                        wxyz=tra.quaternion_from_matrix(transform),
-                        position=transform[:3, 3],
-                    )
-                    self._mesh_handles[geometry_name] = handle
+            ## add mesh
+            handle = self._server.scene.add_mesh_trimesh(
+                name=f"mesh_{i}",
+                mesh=mesh,
+                wxyz=tra.quaternion_from_matrix(transform),
+                position=transform[:3, 3],
+            )
+            self._mesh_handles[geometry_name] = handle
 
-                    handle = self._server.scene.add_frame(
-                        name=f"frame_{i}",
-                        axes_length=0.1,
-                        wxyz=tra.quaternion_from_matrix(transform),
-                        position=transform[:3, 3],
-                        visible=False
-                    )
-                    self._frame_handles[geometry_name] = handle
+            ## add frame            
+            handle = self._server.scene.add_frame(
+                name=f"frame_{i}",
+                axes_length=0.3,
+                axes_radius=0.01,
+                wxyz=tra.quaternion_from_matrix(transform),
+                position=transform[:3, 3],
+                visible=False
+            )
+            self._frame_handles[geometry_name] = handle
 
     def _add_joint_control(self):
+
+        def callback(e: viser.GuiEvent, joint_name: str = None, cfg: dict|list[float] = None):
+            if joint_name is not None:
+                self._urdf_model.update_cfg({joint_name: e.target.value})
+            else:
+                self._urdf_model.update_cfg(configuration=cfg)
+            scene = (
+                self._urdf_model.collision_scene
+                if self._use_collision_mesh
+                else self._urdf_model.scene
+            )
+            for geometry_name, handle in self._mesh_handles.items():
+                transform = scene.graph.get(geometry_name)[0]
+                handle.wxyz = tra.quaternion_from_matrix(transform)
+                handle.position = transform[:3, 3]
+                frame_handle = self._frame_handles[geometry_name]
+                frame_handle.wxyz = tra.quaternion_from_matrix(transform)
+                frame_handle.position = transform[:3, 3]
+
+            if cfg is not None:
+                if isinstance(cfg, dict):
+                    for joint_name, value in cfg.items():
+                        self._slider_handles[joint_name].value = value
+                else:
+                    for joint_name, value in zip(self._urdf_model.actuated_joint_names, cfg):
+                        self._slider_handles[joint_name].value = value
+
         with self._server.gui.add_folder("Controls"):
+
             for joint_name in self._urdf_model.actuated_joint_names:
                 joint = self._urdf_model.joint_map[joint_name]
 
@@ -144,40 +90,28 @@ class Visualizer:
                     step=0.01,
                     initial_value=0.,
                 )
+                self._slider_handles[joint_name] = slider
 
-                def make_callback(joint_name):
-                    def callback(e: viser.GuiEvent):
-                        self._urdf_model.update_cfg({joint_name: e.target.value})
-                        scene = (
-                            self._urdf_model.collision_scene
-                            if self._use_collision_mesh
-                            else self._urdf_model.scene
-                        )
-                        for geometry_name, handle in self._mesh_handles.items():
-                            transform = scene.graph.get(geometry_name)[0]
-                            handle.wxyz = tra.quaternion_from_matrix(transform)
-                            handle.position = transform[:3, 3]
-                            frame_handle = self._frame_handles[geometry_name]
-                            frame_handle.wxyz = tra.quaternion_from_matrix(transform)
-                            frame_handle.position = transform[:3, 3]
+                slider.on_update(partial(callback, joint_name=joint_name))
 
-                    return callback
-
-                slider.on_update(make_callback(joint_name))
+            self._server.gui.add_button("Reset").on_click(partial(callback, cfg=self._urdf_model.zero_cfg))
 
     def _add_link_click(self):
-        for geometry_name, handle in enumerate(self._mesh_handles.items()):
+        for geometry_name, handle in self._mesh_handles.items():
             
-            @handle.on_click
-            def callback(event: viser.SceneNodePointerEvent):
-                handle.mesh.visual.face_colors[:] = [1.0, 0.0, 0.0, 1.0]
-                self._frame_handles[geometry_name].visible = True
-            return callback
+            def make_callback(geometry_name):
+                def callback(event: viser.SceneNodePointerEvent):
+                    # handle.mesh.visual.face_colors[:] = [1.0, 0.0, 0.0, 1.0]
+                    v = self._frame_handles[geometry_name].visible
+                    self._frame_handles[geometry_name].visible = not v
+                return callback
+
+            handle.on_click(make_callback(geometry_name))
 
     def run(self):
         self._add_mesh()
         self._add_joint_control()
-        #self._add_link_click()
+        self._add_link_click()
 
 
 if __name__ == "__main__":
